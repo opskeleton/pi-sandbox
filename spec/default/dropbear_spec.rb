2a14554b0a66800fc53c4202d5f104ead661c6aa
narkisr <narkisr@gmail.com>
2014-04-20 23:01:31 +0300

    dropbear spec

---
+++ spec/default/dropbear_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe package('dropbear') do
+  it { should be_installed }
+end
+
+describe file('/etc/default/dropbear') do
+  it { should be_file }
+  its(:content) { should match /NO_START=0/ }
+  its(:content) { should match /DROPBEAR_PORT=2222/ }
+end
