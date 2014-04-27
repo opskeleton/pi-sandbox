1e787761f92e64c2f924260ef32bb9de4542ec6e
narkisr <narkisr@gmail.com>
2014-04-27 20:01:44 +0300

    ipv6 disable

---
+++ static-modules/pie/manifests/ipv6.pp
@@ -0,0 +1,17 @@
+# disable ipv6
+class pie::ipv6 {
+
+  file{'/etc/sysctl.d/disableipv6.conf':
+    ensure => present
+  } ->
+
+  file_line { 'disableipv6':
+    path => '/etc/sysctl.d/disableipv6.conf',
+    line => 'net.ipv6.conf.all.disable_ipv6=1'
+  }
+
+  file_line { 'disable ipv6 module':
+    path => '/etc/modprobe.d/blacklist',
+    line => 'blacklist ipv6'
+  }
+}
