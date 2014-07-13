9346998bf85ac3ba9dcc6b86078711d86cab9571
narkisr <narkisr@gmail.com>
2014-07-13 03:33:01 +0300

    adding users

---
+++ static-modules/pie/manifests/users.pp
@@ -0,0 +1,27 @@
+# clearing pi user and adding specified user
+class pie::users($user = undef){
+  user{'pi':
+    ensure     => absent,
+    managehome => true,
+    home       => '/home/pi'
+  } ~>
+
+  exec{'purge pi home':
+    command      => 'rm -rf /home/pi',
+    user         => 'root',
+    path         => ['/usr/bin','/bin',],
+    refresh_only => true
+  }
+
+  user{$user:
+    ensure     => present,
+    comment    => 'Local pi admin',
+    managehome => true,
+    home       => "/home/${user}"
+  } ->
+
+  file_line { "${user} sudoer":
+    path => '/etc/sudoers',
+    line => "${user} ALL=(ALL) NOPASSWD: ALL"
+  }
+}
