b4e23e33b7692c7039b77080e20ffeeddcca26d4
narkisr <narkisr@gmail.com>
2014-07-14 02:44:28 +0300

    updated gateway

--- static-modules/pie/manifests/gateway.pp
+++ static-modules/pie/manifests/gateway.pp
@@ -1,10 +1,13 @@
-# making eth0 to wlan0 brigding see http://bit.ly/1mWquA5
+# making eth0 to wlan0 brigding see:
+# 1.http://bit.ly/1mWquA5
+# 2. http://bit.ly/1jpunN6
 class pie::gateway(
-  $wlan_gateway='192.168.4.1',
-  $eth0_address='192.168.5.1',
-  $eth0_subnet='192.168.5.0/24',
+  $address='192.168.5.1',
+  $broadcast='192.168.5.255',
+  $subnet='192.168.5.0/24',
   $dhcp_range ='192.168.5.10,192.168.5.20',
 ){
+
   file { '/etc/network/interfaces':
     ensure  => file,
     mode    => '0644',
@@ -21,9 +24,28 @@ class pie::gateway(
 
   package{'dnsmasq':
     ensure  => present
+  } ->
+
+  file { '/etc/dnsmasq.conf':
+    ensure  => file,
+    mode    => '0644',
+    content => template('pie/dnsmasq.conf.erb'),
+    owner   => root,
+    group   => root,
+  } ~>
+
+  service{'dnsmasq':
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
   }
 
   package{'ifplugd':
     ensure  => absent
+  }
+
+  service{'ifplugd':
+    ensure    => stopped,
+    enable    => false,
   }
 }
