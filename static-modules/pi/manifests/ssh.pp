70d089eacac4deb1ad2459d1ba4a803c36acb8d2
narkisr <narkisr@gmail.com>
2014-08-30 02:45:23 +0300

    fixing dep name

--- static-modules/pi/manifests/ssh.pp
+++ static-modules/pi/manifests/ssh.pp
@@ -27,7 +27,7 @@ class pi::ssh {
       ensure    => running,
       enable    => true,
       hasstatus => true,
-      require   => Editfile::Config['set port 2222']
+      require   => Editfile::Config['dropbear port']
     }
 
     package{'raspi-config':
